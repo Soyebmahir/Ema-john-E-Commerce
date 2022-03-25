db2446e172aa5c4d1d029b023a3e486be67e855c
Soyeb <tssoyeb@gmail.com>
2022-03-25 19:39:51 +0600

    install package add icon change title

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
@@ -20,6 +22,7 @@ const Product = (props) => {
           <div >
               <button onClick={()=>handleAddToCart(product)} className='btn-cart'>
                   Add to cart
+                  <FontAwesomeIcon icon={faShoppingCart} className="icon"></FontAwesomeIcon>
               </button>
           </div>
 
