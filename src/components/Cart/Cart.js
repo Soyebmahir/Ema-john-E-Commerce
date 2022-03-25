0ff5b6ceec56e48ba481232c91bbcac13458e055
Soyeb <tssoyeb@gmail.com>
2022-03-25 23:07:36 +0600

    add calculation on cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,14 +3,24 @@ import './Cart.css'
 
 const Cart = ({cart}) => {
     console.log(cart);
+    let total =0;
+    let shippingCharge =0; 
+    for(const product of cart){
+
+        total = total + product.price;
+        shippingCharge = shippingCharge + product.shipping;
+    }
+    // 10% tex 
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total+shippingCharge+tax;
     return (
         <div className='cart'>
              <h1>Shopping-Cart</h1>
                 <h4>Item Ordered :{cart.length}</h4>
-                <h4>Total Price :</h4>
-                <h4>Total Shipping Charge :</h4>
-                <h4>Total Tax :</h4>
-                <h4>Grand Total :</h4>
+                <h4>Total Price :${total}</h4>
+                <h4>Total Shipping Charge :${shippingCharge}</h4>
+                <h4>Total Tax :${tax}</h4>
+                <h4>Grand Total :${grandTotal}</h4>
         </div>
     );
 };
