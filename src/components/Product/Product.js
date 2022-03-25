108eedb5f9454580e419779fb6927ba882f8ca8a
Soyeb <tssoyeb@gmail.com>
2022-03-25 15:17:41 +0600

    style adding

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {name,img,ratings,price}=props.product
+    const {name,seller,img,ratings,price}=props.product
     console.log(name);
     return (
         <div className='product'>
            <img src={img} alt="" />
-           <p className='product-name'>{name}</p>
+          <div className='product-info'>
+          <p className='product-name'>{name}</p>
            <p className='product-price'>Price:${price}</p>
+           <p><small>Seller :{seller}</small></p>
+           <p><small>Rating: {ratings} stars</small></p>
+          </div>
+          <div className='btn-cart'>
+              <button>
+                  Add to cart
+              </button>
+          </div>
 
         </div>
     );
